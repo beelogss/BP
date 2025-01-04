0a6fa01ff9435b96fa602478fc7f92ea0713121b
bilogsWorkspace <lopezgerard750@gmail.com>
2025-01-04 15:16:46 +0800

    index.js ulit huhu

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ const serviceAccount = {
   type: process.env.FIREBASE_TYPE,
   project_id: process.env.FIREBASE_PROJECT_ID,
   private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-  private_key: process.env.FIREBASE_PRIVATE_KEY, // Handle newlines
+  private_key: privateKey.replace(/\\n/g, "\n"), // Handle newlines
   client_email: process.env.FIREBASE_CLIENT_EMAIL,
   client_id: process.env.FIREBASE_CLIENT_ID,
   auth_uri: process.env.FIREBASE_AUTH_URI,
@@ -34,8 +34,18 @@ admin.initializeApp({
 const db = admin.firestore();
 // const storage = new Storage({ keyFilename: "./serviceAccountKey.json" });
 const storage = new Storage({
-  credentials: serviceAccount,
-  projectId: process.env.FIREBASE_PROJECT_ID,
+  credentials: {
+    type: process.env.FIREBASE_TYPE,
+    project_id: process.env.FIREBASE_PROJECT_ID,
+    private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+    private_key: process.env.FIREBASE_PRIVATE_KEY,
+    client_email: process.env.FIREBASE_CLIENT_EMAIL,
+    client_id: process.env.FIREBASE_CLIENT_ID,
+    auth_uri: process.env.FIREBASE_AUTH_URI,
+    token_uri: process.env.FIREBASE_TOKEN_URI,
+    auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+    client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL
+  }
 });
 const bucket = storage.bucket("bpts-34c54.appspot.com"); // Replace with your Firebase Storage bucket name
 
